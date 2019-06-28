e62936bfe54398dd21c7a0702bb4711ff44610f8
Eric Horodyski <eric@horodyski.me>
2019-06-27 21:10:53 -0400

    Start Saving Category Endpoint

--- Supermarket.API/Controllers/CategoriesController.cs
+++ Supermarket.API/Controllers/CategoriesController.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Services;
+using Supermarket.API.Extensions;
+using Supermarket.API.Resources;
 
 namespace Supermarket.API.Controllers
 {
@@ -10,17 +13,30 @@ namespace Supermarket.API.Controllers
   public class CategoriesController : Controller
   {
     private readonly ICategoryService _categoryService;
+    private readonly IMapper _mapper;
 
-    public CategoriesController(ICategoryService categoryService)
+    public CategoriesController(
+      ICategoryService categoryService,
+      IMapper mapper
+      )
     {
       _categoryService = categoryService;
+      _mapper = mapper;
     }
 
     [HttpGet]
-    public async Task<IEnumerable<Category>> GetAllAsync()
+    public async Task<IEnumerable<CategoryResource>> GetAllAsync()
     {
       var categories = await _categoryService.ListAsync();
-      return categories;
+      var resources = _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryResource>>(categories);
+      return resources;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync([FromBody] SaveCategoryResource resource)
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState.GetErrorMessages());
     }
   }
 }
